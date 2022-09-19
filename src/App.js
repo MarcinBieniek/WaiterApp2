b2491a0aab5fd4a9f858e2caa6146d1ff6a3d936
Marcin Bieniek <marcingim@gmail.com>
2022-09-19 11:07:49 +0200

    Add Provider and React Router connections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import React from 'react';
 
+import { fetchTables } from './redux/tablesRedux';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
+import { Container } from 'react-bootstrap';
+
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
+import Tables from './components/pages/Tables/Tables';
+import EditTableForm from './components/features/EditTableForm/EditTableForm';
+import NotFound from './components/pages/NotFound';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(fetchTables()), [dispatch]);
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <main>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Tables />} />
+          <Route path='/table/:id' element={<EditTableForm />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Container>
+    </main>
   );
 }
 
